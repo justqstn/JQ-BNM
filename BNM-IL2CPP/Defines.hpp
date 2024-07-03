fe734744b0fd4ec94bad3634e974b27c105cf3f4
justqstn <justqstn@gmail.com>
2024-07-03 17:26:57 +0300

    Added examples

--- BNM-IL2CPP/Defines.hpp
+++ BNM-IL2CPP/Defines.hpp
@@ -1,10 +1,13 @@
-#include "../Logger/Logger.h"
 
-// Debug function
-#define LOG_DEBUG Logger::log_debug
+// Log functions.
+void __log(const char *msg)
+{
+    return;
+}
+#define LOG_DEBUG __log
 
 // Error function
-#define LOG_ERROR Logger::log_err
+#define LOG_ERROR __log
 
 #ifdef _WIN64
 #define IL2CPP_CALLING_CONVENTION __fastcall *
